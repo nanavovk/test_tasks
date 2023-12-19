3462b79d6b483447a1bb980ce28ddd612fb66daa
nanavovk <nanavovk111@gmail.com>
2023-12-19 20:51:47 +0300

    test tasks

--- task3/main3.py
+++ task3/main3.py
@@ -12,23 +12,21 @@ with open(args.values_path, 'r') as file1:
     values = json.load(file1)
 
 report = tests
-print(tests['tests'])
-
 def setvals(a: dict, b: dict) -> dict:
     for test in a:
         for value in b['values']:
             if test['id'] == value['id']:
                 test['value'] = value['value']
-                print(test['value'])
         if test.keys().__contains__('values'):
             setvals(test['values'], b)
 
 setvals(tests['tests'], values)
-print(tests['tests'])
 
 report = {'tests': tests['tests']}
 
 with open('report.json', 'w') as file2:
     json.dump(report, file2, indent=2)
+
+print('Завершено')
 
 
